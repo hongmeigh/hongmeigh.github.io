ed1f1f871da621c596f70744ce276f3c3cebeabb
hongmei.gao <hongmei.gao@zhan.com>
2016-09-21 18:23:23 +0800

    提交

---
+++ js/app1.js
@@ -0,0 +1,55 @@
+$(function(){
+	var ballcount=3;
+	$(document).mousedown(function(e){
+		window.pand=true;
+		var x=e.pageX;
+		var y=e.pageY;		
+		count=count+1;
+		clickbg(x,y,count,ballcount);
+	})
+	$(document).mousemove(function(e){
+		if(window.pand){
+			window.pand=true;
+			var x=e.pageX;
+			var y=e.pageY;		
+			count=count+1;
+			clickbg(x,y,count,ballcount);
+		}
+	})
+	$(document).mouseup(function(){
+		window.pand=false;
+	})
+})
+var color=["red","yellow","green","blue","orange","purple","pink","white","gray","darkgree","lightblue"];
+var r=100;
+var count=0;
+function clickbg(x,y,count,ballcount){
+	var ballcount1=ballcount+Math.round(Math.random()*20);
+	var htmlobj=[],deg=[],tasy=[],tasx=[]; 
+   	var webkitStyles = '-webkit-transition' in document.documentElement.style;
+   	var TRANSFORM_NAME = webkitStyles ? 'webkitTransform' : 'transform'; 
+   	var rn=r*(Math.random()*2+0.2);	
+	$("body").append("<div class='div"+count+"'></div>");
+	for(var j=0; j<ballcount1; j++){
+		var n=Math.round(Math.random()*10);
+		$(".div"+count).append("<div class='"+color[n]+" circle dongh"+j+"'></div>");
+	}
+	$(".div"+count+" "+".circle").css({"top":y,"left":x});	
+	var divcount=$(".div"+count);
+	for(var i=0; i<ballcount1; i++){		
+		htmlobj[i]=$(".div"+count+" "+".dongh"+i);
+		deg[i]=2*Math.PI/ballcount1*i;
+		tasy[i]=rn*Math.sin(deg[i]);
+		tasx[i]=rn*Math.cos(deg[i]);
+		setTimeout(
+			function(){
+				for(var n=0; n<ballcount1; n++){
+					htmlobj[n][0].style[TRANSFORM_NAME]="translate("+tasx[n]+"px,"+tasy[n]+"px)";
+				}
+		},5)
+	}
+	setTimeout(function(){divcount.remove();},550)
+
+	
+	
+}
